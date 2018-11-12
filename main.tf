3d4e3b2921dec0354d285a0fc3dfec2717537cdd
Ethan Devenport <edevenport@users.noreply.github.com>
2018-11-13 00:14:53 +0100

    Adjusted ssh_key conditional.

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ data "nks_keyset" "ssh_keyset" {
 
 resource "nks_keyset" "ssh_keyset" {
   count    = "${var.ssh_key_path == "" ? 0 : 1}"
-  name     = "${var.cluster_name} SHH Key"
+  name     = "${var.cluster_name} SSH Key"
   org_id   = "${data.nks_organization.default.id}"
   category = "user_ssh"
   entity   = "aws"
@@ -64,7 +64,7 @@ resource "nks_cluster" "cluster" {
   platform              = "${var.platform}"
   channel               = "stable"
   project_id            = "${var.project_id}"
-  ssh_keyset            = "${var.ssh_key_path == "" ? data.nks_keyset.ssh_keyset.id : nks_keyset.ssh_keyset.id}"
+  ssh_keyset            = "${var.ssh_key_path == "" ? "${join("", data.nks_keyset.ssh_keyset.*.id)}" : "${join("", nks_keyset.ssh_keyset.*.id)}"}"
 }
 
 resource "local_file" "kubeconfig" {
